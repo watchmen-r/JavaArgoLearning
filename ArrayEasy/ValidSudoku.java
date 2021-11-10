b5da08d0d3337fbcacf6c5baa6ef53fcf55c4ef6
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2021-11-11 00:22:33 +0900

    review valid sudoku

--- ArrayEasy/ValidSudoku.java
+++ ArrayEasy/ValidSudoku.java
@@ -1,28 +1,50 @@
 package ArrayEasy;
 
-import java.util.HashSet;
-import java.util.Set;
-
 class ValidSudoku {
-  public static void main(String[] args) {
-    // 入力が多いのでスキップ
-  }
-  
-  // TODO ×解きなおし
-  static boolean isValidSudoku(char[][] board) {
-    Set<String> set = new HashSet<String>();
-    for(int i = 0; i < 9; i++) {
-      for(int j = 0; j < 9; j++) {
-        char numChar = board[i][j];
-        if(numChar != '.') {
-          if(!set.add(numChar + " in row " + i) ||
-             !set.add(numChar + " in column " + j) ||
-             !set.add(numChar + " in block " + i/3 + "-" + j/3)){
-              return false;
-             }
-        }
-      }
+    public static void main(String[] args) {
+        // 入力が多いのでスキップ
     }
-    return true;
-  }
+
+    static boolean isValidSudoku(char[][] board) {
+        int n = 9;
+
+        int[][] rows = new int[n][n];
+        int[][] cols = new int[n][n];
+        int[][] boxes = new int[n][n];
+
+        // rは行
+        for (int r = 0; r < n; r++) {
+            // cは列
+            for (int c = 0; c < n; c++) {
+                // .だったら次
+                if (board[r][c] == '.') {
+                    continue;
+                }
+
+                // 数を配列の索引にする
+                // charの１を引くことでintで1を引いた数になる
+                int pos = board[r][c] - '1';
+
+                // 行に値がすでにあった場合
+                if (rows[r][pos] == 1) {
+                    return false;
+                }
+                rows[r][pos] = 1;
+
+                // 列に値がすでにあった場合
+                if (cols[c][pos] == 1) {
+                    return false;
+                }
+                cols[c][pos] = 1;
+
+                // 3×3に値がすでにあった場合
+                int boxIndex = (r / 3) * 3 + c / 3;
+                if (boxes[boxIndex][pos] == 1) {
+                    return false;
+                }
+                boxes[boxIndex][pos] = 1;
+            }
+        }
+        return true;
+    }
 }
