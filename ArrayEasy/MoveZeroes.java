5d3de4bc460a0023dcefe7c0a2f523a8b8e6c884
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2021-11-10 23:31:38 +0900

    learn move zeroes and two sum

--- ArrayEasy/MoveZeroes.java
+++ ArrayEasy/MoveZeroes.java
@@ -1,22 +1,20 @@
 package ArrayEasy;
 
 class MoveZeros {
-  public static void main(String[] args) {
-      int nums[] = { 1, 1, 2 };
-      movezeroes(nums);
-
-  }
-
-  // OK
-  public static void movezeroes(int[] nums) {
-    int zeroSequence = 0;
-    for(int i = 0; i < nums.length; i++) {
-      if(nums[i] == 0) {
-        zeroSequence++;
-      } else if(nums[i] != 0 && zeroSequence != 0){
-        nums[i - zeroSequence] = nums[i];
-        nums[i] = 0;
-      }
+    public static void main(String[] args) {
+        int nums[] = { 1, 1, 2 };
+        movezeroes(nums);
     }
-  }
+
+    public static void movezeroes(int[] nums) {
+        int zeroSequence = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zeroSequence++;
+            } else if (nums[i] != 0 && zeroSequence != 0) {
+                nums[i - zeroSequence] = nums[i];
+                nums[i] = 0;
+            }
+        }
+    }
 }
--- ArrayEasy/TwoSum.java
+++ ArrayEasy/TwoSum.java
@@ -4,37 +4,37 @@ import java.util.HashMap;
 import java.util.Map;
 
 class TwoSum {
-  public static void main(String[] args) {
-    int[] nums = {1,2,3,4};
-    int target = 3;
-    twoSum(nums, target);
-    twoSumAnother(nums, target);
-  }
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4 };
+        int target = 3;
+        twoSum(nums, target);
+        twoSumAnother(nums, target);
+    }
 
-  // 有名問題、anotherがわからなかったので解き直す
-  static int[] twoSum(int[] nums, int target) {
-    for(int i = 0; i < nums.length; i++) {
-      for(int j = i + 1; j < nums.length; j++) {
-        if((nums[i] + nums[j]) == target) {
-          int[] answer = {i, j};
-          return answer;
+    // 有名問題、anotherがわからなかったので解き直す
+    static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    int[] answer = { i, j };
+                    return answer;
+                }
+            }
         }
-      }
+        return null;
     }
-    return null;
-  }
 
-  static int[] twoSumAnother(int[] nums, int target) {
-    int[] answer = new int[2];
-    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-    for(int i = 0; i < nums.length; i++) {
-      if(map.containsKey(target - nums[i])) {
-        answer[1] = i;
-        answer[0] = map.get(target - nums[i]);
+    static int[] twoSumAnother(int[] nums, int target) {
+        int[] answer = new int[2];
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                answer[1] = i;
+                answer[0] = map.get(target - nums[i]);
+                return answer;
+            }
+            map.put(nums[i], i);
+        }
         return answer;
-      }
-      map.put(nums[i], i);
     }
-    return answer;
-  }
 }
