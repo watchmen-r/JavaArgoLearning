af86ba7d1d75ad4254a6b3e5a6952360107cf161
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2021-11-12 00:43:06 +0900

    review valid anagram

--- StringsEasy/FirstUniqueChar.java
+++ StringsEasy/FirstUniqueChar.java
@@ -1,27 +1,29 @@
 package StringsEasy;
+
 import java.util.Map;
 import java.util.LinkedHashMap;
 
 class FirstUniqueChar {
-  public static void main(String[] args) {
-    String s = "loveleetcode";
-    firstUniqueChar(s);
-  }
+    public static void main(String[] args) {
+        String s = "loveleetcode";
+        firstUniqueChar(s);
+    }
 
-  static int firstUniqueChar(String s) {
-    Map<Character, Integer> map = new LinkedHashMap<>();
-    for(int i = 0; i < s.length(); i++) {
-        char c = s.charAt(i);
-        if(map.containsKey(c)) {
-            map.put(c, map.get(c) + 1);
-        } else {
-            map.put(c, 1);
+    static int firstUniqueChar(String s) {
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
         }
-    }
 
-    for(int i = 0; i < s.length(); i++) {
-        if(map.get(s.charAt(i)) == 1) return i;
+        for (int i = 0; i < s.length(); i++) {
+            if (map.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
     }
-    return -1;
-  }
 }
--- StringsEasy/ValidAnagram.java
+++ StringsEasy/ValidAnagram.java
@@ -1,29 +1,26 @@
 package StringsEasy;
 
 class ValidAnagram {
-  public static void main(String[] args) {
-    String s = "rat";
-    String t = "cat";
-    System.out.println(validAnagram(s, t));
-  }
-
-  // NG 解きなおし 発想は難しい
-  static boolean validAnagram(String s, String t) {
-    if(s.length()!=t.length()){
-      return false;
+    public static void main(String[] args) {
+        String s = "rat";
+        String t = "cat";
+        System.out.println(validAnagram(s, t));
     }
 
-    int[] count = new int[26];
-    for(int i = 0; i < s.length(); i++) {
-      // asciiでa=97・・・z=122、引き算することでintにしている
-      count[s.charAt(i)-'a']++;
-      count[t.charAt(i)-'a']--;
+    static boolean validAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+        for(int i = 0; i < s.length(); i++) {
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+
+        for(int i : count) {
+            if(i!=0) return false;
+        }
+        return true;
     }
-    for(int i : count) {
-      if(i!=0) {
-        return false;
-      }
-    }
-    return true;
-  }
 }
