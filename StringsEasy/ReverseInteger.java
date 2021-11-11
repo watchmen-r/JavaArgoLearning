6270091907275422775d8d4a0c227df5e99fd7d7
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2021-11-11 23:37:04 +0900

    review reverseInteger

--- StringsEasy/ReverseInteger.java
+++ StringsEasy/ReverseInteger.java
@@ -1,25 +1,23 @@
 package StringsEasy;
 
 class ReverseInteger {
-  public static void main(String[] args) {
-    int n = 123;
-    reverseInteger(n);
-  }
-
-  // NG 解きなおし 発想は難しい
-  // また、負の数の剰余は言語によって正になったり負になったりするらしい
-  // Javaは負になる
-  static int reverseInteger(int x) {
-    int answer = 0;
-    while(x != 0) {
-      int tail = x % 10;
-      int newResult = answer * 10 + tail;
-
-      if((newResult - tail) / 10 != answer) return 0;
-
-      answer = newResult;
-      x /= 10;
+    public static void main(String[] args) {
+        int n = 123;
+        reverseInteger(n);
     }
-    return answer;
-  }
+
+    static int reverseInteger(int x) {
+        int answer = 0;
+        while(x != 0) {
+            int tail = x % 10;
+            int newResult = answer * 10 + tail;
+
+            // overflowがあった場合を検知(intでマックスに入れられたら、反対にするとoverflowする)
+            if((newResult - tail) / 10 != answer) return 0;
+
+            answer = newResult;
+            x /= 10;
+        }
+        return answer;
+    }
 }
