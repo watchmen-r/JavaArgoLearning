db97fdfe90caa894df76ce3618f17e9d72c1bb7d
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2021-11-12 23:45:38 +0900

    review valid palindrome

--- StringsEasy/ValidPalindrome.java
+++ StringsEasy/ValidPalindrome.java
@@ -1,32 +1,28 @@
 package StringsEasy;
 
 class ValidPalindrome {
-  public static void main(String[] args) {
-    String s = "0P";
-    System.out.println(isPalindrome(s));
-  }
-
-  // TODO 解きなおし
-  static boolean isPalindrome(String s) {
-    int head = 0;
-    int tail = s.length()-1;
-    char cHead, cTail;
-
-    while(head <= tail) {
-      cHead = s.charAt(head);
-      cTail = s.charAt(tail);
-      if(!Character.isLetterOrDigit(cHead)) {
-        head++;
-      } else if(!Character.isLetterOrDigit(cTail)) {
-        tail--;
-      } else {
-        if(Character.toLowerCase(cHead) != Character.toLowerCase(cTail)) {
-          return false;
-        }
-        head++;
-        tail--;
-      }
+    public static void main(String[] args) {
+        String s = "0P";
+        System.out.println(isPalindrome(s));
     }
-    return true;
-  }
+
+    static boolean isPalindrome(String s) {
+        int head = 0;
+        int tail = s.length() - 1;
+
+        while (head <= tail) {
+            while (head < tail && !Character.isLetterOrDigit(s.charAt(head))) {
+                head++;
+            }
+            while (head < tail && !Character.isLetterOrDigit(s.charAt(tail))) {
+                tail--;
+            }
+            if (Character.toLowerCase(s.charAt(head)) != Character.toLowerCase(s.charAt(tail)))
+                return false;
+            
+            head++;
+            tail--;
+        }
+        return true;
+    }
 }
