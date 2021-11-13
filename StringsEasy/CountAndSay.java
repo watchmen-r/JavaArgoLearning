b482f6686ca70182baa2be4d714c8159d94ac9d8
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2021-11-13 17:46:42 +0900

    review count and say

--- StringsEasy/CountAndSay.java
+++ StringsEasy/CountAndSay.java
@@ -1,31 +1,27 @@
 package StringsEasy;
 
 class CountAndSay {
-  public static void main(String[] args) {
-    int n = 4;
-    System.out.println(countAndSay(n));
-  }
-
-  // もうちょっとで自力だった 再度解き直す
-  static String countAndSay(int n) {
-    String answer = "1";
-    for(int i = 1; i < n; i++) {
-      char tmp = answer.charAt(0);
-      int count = 1;
-      String tmpAns = "";
-
-      for(int j = 1; j < answer.length(); j++) {
-        if(tmp == answer.charAt(j)) {
-          count++;
-        } else {
-          tmpAns = tmpAns + String.valueOf(count) + tmp;
-          tmp = answer.charAt(j);
-          count = 1;
-        }
-      }
-      answer = tmpAns + String.valueOf(count) + tmp;
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(countAndSay(n));
     }
 
-    return answer;
-  }
+    static String countAndSay(int n) {
+        String answer = "1";
+        for (int i = 1; i < n; i++) {
+            String newAnswer = "";
+            for (int j = 0; j < answer.length(); j++) {
+                char firstChar = answer.charAt(j);
+                int count = 1;
+                while (j + 1 < answer.length() && answer.charAt(j + 1) == firstChar) {
+                    count++;
+                    j++;
+                }
+                newAnswer = newAnswer + String.valueOf(count) + firstChar;
+            }
+            answer = newAnswer;
+        }
+
+        return answer;
+    }
 }
