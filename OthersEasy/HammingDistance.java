21f6b5c231410067b9e592a62904dae120301c32
Hisanobu Oda <watchmen0205@yahoo.co.jp>
2021-11-25 00:36:30 +0900

    review hamming distance

--- OthersEasy/HammingDistance.java
+++ OthersEasy/HammingDistance.java
@@ -5,7 +5,6 @@ class HammingDistance {
         System.out.println(hammingDistance(1, 4));
     }
 
-    // 解きなおし、ハミング距離について
     static int hammingDistance(int x, int y) {
         return Integer.bitCount(x ^ y); 
     }
